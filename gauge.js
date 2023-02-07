15817e85f91039b7c7f1594597965ee05432d2e7
Nishalini02 <72751447+Nishalini02@users.noreply.github.com>
2023-02-07 18:26:15 +0530

    Update gauge.js

--- gauge.js
+++ gauge.js
@@ -122,28 +122,7 @@ var getScriptPromisify = (src) => {
 			
 			this.render(this.$value, this.$info, this.$color);
 		}
-      set myDataSource(dataBinding) {
-      this._myDataSource = dataBinding;
-      this.render();
-    }
-
-    async render() {
-      await getScriptPromisify(
-        "https://cdn.staticfile.org/echarts/5.0.0/echarts.min.js"
-      );
-
-      if (!this._myDataSource || this._myDataSource.state !== "success") {
-        return;
-      }
-
-      const dimension = this._myDataSource.metadata.feeds.dimensions.values[0];
-      const measure = this._myDataSource.metadata.feeds.measures.values[0];
-      const data = this._myDataSource.data.map((data) => {
-        return {
-          name: data[dimension].label,
-          value: data[measure].raw,
-        };
-      });
+  
 	}
 	
 	customElements.define("com-demo-gauge", Box);
