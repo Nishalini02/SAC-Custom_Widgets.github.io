cb0e8e86877270f1bf6ba1a4f5cbbb7ca5040729
Nishalini02 <72751447+Nishalini02@users.noreply.github.com>
2023-02-13 21:45:53 +0530

    Update progressbar.js

--- progressbar.js
+++ progressbar.js
@@ -1,3 +1,8 @@
+var getScriptPromisify = (src) => {
+  return new Promise((resolve) => {
+    $.getScript(src, resolve);
+  });
+};
 (function() { 
 	let template = document.createElement("template");
 	template.innerHTML = `
@@ -63,43 +68,20 @@
 			super(); 
 			let shadowRoot = this.attachShadow({mode: "open"});
 			shadowRoot.appendChild(template.content.cloneNode(true));
-			this._root = this._shadowRoot.getElementById("root");
 			this.$style = shadowRoot.querySelector('style');			
 			this.$svg = shadowRoot.querySelector('svg');
-			
-			this.addEventListener("click", event => {
-				var event = new Event("onClick");
-				this.dispatchEvent(event);
-			});
+			this._root = this._shadowRoot.getElementById("root");
 			
 			this._props = {};
+			this._props = {};
+
+      this.render();
 		}
+		onCustomWidgetResize(width, height) {
+      this.render();
+    }
+
 		
-		render(val,valll ,valul, info, color) {
-			var val1 = val ;
-			var valll1 = valll;
-			var valul1 = valul;
-			var x = this.progress(val1,valll1,valul1);
-			var rounded = Math.round( val * 10 ) / 10;
-			var percentage = (val1/(valul1-valll1))*100;
-
-			
-			if(rounded >=0 && rounded <=100) {
-				this.$style.innerHTML = ':host {border-radius: 10px;border-width: 2px;border-color: white;border-style: solid;display: block;}.body {background: #fff;}.metric {padding: 10%;}.metric svg {max-width: 100%;}.metric path {stroke-width: 75;stroke: #ecf0f1;fill: none;}.metric text {font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;}.metric.participation path.data-progress {stroke: ' + color + ';}.metric.participation text {fill: ' + color + ';}';
-				this.$svg.innerHTML = '<path d="M 50 500 L950 500"></path><text class="percentage" text-anchor="middle" alignment-baseline="middle" x="500" y="200" font-size="140" font-weight="bold">' + rounded +'( '+percentage + '% )</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="350" font-size="90" font-weight="normal">' + info + '</text><path d="' + x + '" class="data-progress"></path><text class="title" text-anchor="middle" alignment-baseline="middle" x="25" y="485" font-size="20" font-weight="normal">' + valll1 + '</text><text class="title" text-anchor="middle" alignment-baseline="middle" x="975" y="485" font-size="20" font-weight="normal">' + valul1 + '</text>"';
-			}
-		}
-
-		  
-		progress(x,ll,ul){
-			var end_x;
-			end_x=(900*(x/(ul-ll)))+50;
-			return "M 50 500 L"+ end_x+" 500";
-		};
-
-		onCustomWidgetBeforeUpdate(changedProperties) {
-			this._props = { ...this._props, ...changedProperties };
-		}
                set myDataSource(dataBinding) {
                this._myDataSource = dataBinding;
                this.render();
@@ -107,6 +89,11 @@
 		if (!this._myDataSource || this._myDataSource.state !== "success") {
         return;
       }
+		async render() {
+      await getScriptPromisify(
+        "https://cdn.staticfile.org/echarts/5.0.0/echarts.min.js"
+      );
+
 
       const dimension = this._myDataSource.metadata.feeds.dimensions.values[0];
       const measure = this._myDataSource.metadata.feeds.measures.values[0];
@@ -116,30 +103,58 @@
           value: data[measure].raw,
         };
       });
-		onCustomWidgetAfterUpdate(changedProperties) {
-			if ("value" in changedProperties) {
-				this.$value = changedProperties["value"];
-			}
-			
-			if ("LowerLimitvalue" in changedProperties) {
-				this.$LowerLimitvalue = changedProperties["LowerLimitvalue"];
-			}
-			
-			if ("UpperLimitvalue" in changedProperties) {
-				this.$UpperLimitvalue = changedProperties["UpperLimitvalue"];
-			}
-			
-			if ("info" in changedProperties) {
-				this.$info = changedProperties["info"];
-			}
-			
-			if ("color" in changedProperties) {
-				this.$color = changedProperties["color"];
-			}
-			
-			this.render(this.$value,this.$LowerLimitvalue,this.$UpperLimitvalue, this.$info, this.$color);
-		}
-	}
+      const myChart = echarts.init(this._root, "wight");
+      const option = {
+        title: {
+          text: "ProgressBar",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c}%",
+        },
+        toolbox: {
+          feature: {
+            dataView: { readOnly: false },
+            restore: {},
+            saveAsImage: {},
+          },
+        },
+        legend: {
+          data: ["Show", "Click", "Visit", "Inquiry", "Order"],
+        },
+        series: [
+          {
+            name: "ProgressBar",
+            type: "bar",
+            left: "10%",
+            width: "80%",
+            min: 0,
+            max: 100,
+            minSize: "0%",
+            maxSize: "100%",
+            labelLine: {
+              length: 10,
+              lineStyle: {
+                width: 1,
+                type: "solid",
+              },
+            },
+            itemStyle: {
+              borderColor: "#fff",
+              borderWidth: 1,
+            },
+            emphasis: {
+              label: {
+                fontSize: 20,
+              },
+            },
+            data,
+          },
+        ],
+      };
+      myChart.setOption(option);
+    }
+  }
 	
 	customElements.define("com-demo-progressbar", Box);
 })();
